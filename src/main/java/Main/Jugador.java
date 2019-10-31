8cf97b3b3c94d5fe1b8286a79296015483dc1098
GelosoFederico <gelosofederico@gmail.com>
2019-10-30 21:47:26 -0300

    Primer commit

---
+++ src/main/java/Main/Jugador.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class Jugador 
+{
+    private String _nombre;
+
+    public String nombre() {
+        return this._nombre;
+    }
+
+    public void nombre(String nombre) {
+        this._nombre = nombre;
+    }
+}
