a9efa969c137a4f0542414b8aaa89bb1c4e1824a
ctorales <crist.torales@gmail.com>
2019-11-24 20:15:09 -0300

    Prueba inicial, falta probar el handler.

--- src/main/java/Main/App.java
+++ src/main/java/Main/App.java
@@ -12,12 +12,16 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
-
+    // --module-path "C:\Program Files\Java\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml
+    // TODO sin esto en la opcion de la VM no anda
+    // Preguntar
     @Override
     public void start(Stage stage) {
-
-
-        stage.show();
+       Button botonDeInicio =  new Button();
+       botonDeInicio.setText("Enviar");
+       Scene scene = new Scene(botonDeInicio);
+       stage.setScene(scene);
+       stage.show();
     }
 
     public static void main(String[] args) {
