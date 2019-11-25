5147cea941b1b448c1827a66a49968761cd205a6
ctorales <crist.torales@gmail.com>
2019-11-25 01:09:57 -0300

    Interfaz básica: Ahora se puede ver el tablero de 20x20.

--- src/main/java/Main/InterfazGrafica/App.java
+++ src/main/java/Main/InterfazGrafica/App.java
@@ -17,18 +17,25 @@ public class App extends Application {
     // --module-path "C:\Program Files\Java\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml
     // TODO sin esto en la opcion de la VM no anda
     // Preguntar: ya se cargó el archivo de JavaFx en el proyecto.
+
+    private static final int LONGITUD = 20;
+    private static final int ALTURA_CASILLERO = 32;
+    private static final int ANCHURA_CASILLERO = 32;
+
     @Override
     public void start(Stage stage) {
-       Button botonDeInicio =  new Button();
-       botonDeInicio.setText("Inicio del juego");
-
-     //  Scene scene = new Scene(botonDeInicio);
+        //Button botonDeInicio =  new Button();
+        // botonDeInicio.setText("Inicio del juego");
+        //  Scene scene = new Scene(botonDeInicio);
 
         GridPane grid = new GridPane();
 
-        for(int i=0; i< 20; i++) {
-            for (int j = 0; j < 20; j++){
-                grid.add(new Button("T"), i, j);
+        for(int i=0; i< LONGITUD; i++) {
+            for (int j = 0; j < LONGITUD; j++){
+                ImageView imagenCasillero;
+                grid.add(imagenCasillero = new ImageView("file:src/imagenCasillero.png"), i, j);
+                imagenCasillero.setFitHeight(ALTURA_CASILLERO);
+                imagenCasillero.setFitWidth(ANCHURA_CASILLERO);
             }
         }
 
