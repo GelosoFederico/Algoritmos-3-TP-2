0f265468c3e2c81561b7e81d13b3ec0cb3cc25e0
ctorales <crist.torales@gmail.com>
2019-11-24 22:07:31 -0300

    Se agregaron bibliotecas para la portabilidad de Javafx.

--- src/main/java/Main/App.java
+++ src/main/java/Main/App.java
@@ -1,9 +1,11 @@
 package Main;
 
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -18,10 +20,21 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
        Button botonDeInicio =  new Button();
-       botonDeInicio.setText("Enviar");
-       Scene scene = new Scene(botonDeInicio);
-       stage.setScene(scene);
-       stage.show();
+       botonDeInicio.setText("Inicio del juego");
+
+     //  Scene scene = new Scene(botonDeInicio);
+
+        GridPane grid = new GridPane();
+
+        for(int i=0; i< 20; i++) {
+            for (int j = 0; j < 20; j++){
+                grid.add(new Button("Critian"), i, j);
+            }
+        }
+
+        Scene scene = new Scene(grid);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
