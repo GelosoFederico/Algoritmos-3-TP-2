c73df9edc6327a8cd4848d6c6f67f44fab8a4693
Cristina <ckuo@fi.uba.ar>
2019-12-01 17:57:45 -0300

    Poner un StackPane a cada casillero del GridPane

--- src/main/java/main/vista/GridPaneTablero.java
+++ src/main/java/main/vista/GridPaneTablero.java
@@ -2,6 +2,7 @@ package main.vista;
 
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import main.controlador.HandlerTablero;
 
 public class GridPaneTablero extends GridPane {
@@ -14,8 +15,10 @@ public class GridPaneTablero extends GridPane {
 
         for(int i=0; i< LONGITUD; i++) {
             for (int j = 0; j < LONGITUD; j++){
-                ImageView imagenCasillero;
-                this.add(imagenCasillero = new ImageView("file:src/imagenCasillero.png"), i, j);
+                ImageView imagenCasillero = new ImageView("file:src/imagenCasillero.png");
+                StackPane stackPane = new StackPane();
+                stackPane.getChildren().add(imagenCasillero);
+                this.add(stackPane,i,j);
                 imagenCasillero.setFitHeight(ALTURA_CASILLERO);
                 imagenCasillero.setFitWidth(ANCHURA_CASILLERO);
             }
