034667d3df1d72baa0714a894321f6ebb9cb96ac
ctorales <crist.torales@gmail.com>
2019-12-08 18:29:45 -0300

    Se eliminó la herencia, se agregó la clase abstracta AtaqueLejano.

--- src/main/java/main/modelo/unidad/ataque/AtaqueLejanoNormal.java
+++ src/main/java/main/modelo/unidad/ataque/AtaqueLejanoNormal.java
@@ -1,14 +1,15 @@
 package main.modelo.unidad.ataque;
 
-import main.modelo.tablero.Tablero;
 import main.modelo.tablero.distancia.Lejana;
+import main.modelo.tablero.Tablero;
 import main.modelo.unidad.Unidad;
 
-public abstract class AtaqueLejano extends AtaqueEstrategia  {
-    AtaqueLejano(int danio) {
-        super(danio);
-    }
+public class AtaqueLejanoNormal extends AtaqueLejano {
 
+
+    public AtaqueLejanoNormal(int danio) { super(danio); }
+
+    @Override
     public void atacar(Unidad atacante, Unidad victima) {
         Tablero.getInstance().daniarUnidadEnRango(atacante, victima, new Lejana(), danio);
     }
