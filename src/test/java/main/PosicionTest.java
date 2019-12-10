a3ebc153c77aa8962dbe0adc257e8c2069ee2433
Cristina <ckuo@fi.uba.ar>
2019-12-10 17:46:06 -0300

    Posicion tiene un metodo para saber cual es la posicion relativa (direccion) de otra posicion.
    Si esta no esta a distancia 1 se tira una excepcion.

--- src/test/java/main/PosicionTest.java
+++ src/test/java/main/PosicionTest.java
@@ -1,8 +1,12 @@
 package main;
 
+import main.modelo.direccion.Direccion;
+import main.modelo.direccion.Norte;
+import main.modelo.direccion.Oeste;
+import main.modelo.direccion.Sureste;
 import main.modelo.tablero.distancia.Posicion;
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 
 public class PosicionTest {
@@ -45,4 +49,42 @@ public class PosicionTest {
         Assert.assertEquals(distancia,10);
     }
 
+    @Test
+    public void posicion11PosicionRelativaAPosicion10DevuelveOeste() {
+        // Arrange
+        Posicion posicion = new Posicion(1,1);
+        Posicion otraPosicion = new Posicion(1,0);
+        Direccion direccionEsperada = new Oeste();
+        // Act
+        Direccion direccionObtenida = posicion.posicionRelativaA(otraPosicion);
+
+        // Assert
+        Assert.assertTrue((direccionEsperada.posicionRelativa()).igualA(direccionObtenida.posicionRelativa()));
+    }
+
+    @Test
+    public void posicion55PosicionRelativaAPosicion66DevuelveSureste() {
+        // Arrange
+        Posicion posicion = new Posicion(5,5);
+        Posicion otraPosicion = new Posicion(6,6);
+        Direccion direccionEsperada = new Sureste();
+        // Act
+        Direccion direccionObtenida = posicion.posicionRelativaA(otraPosicion);
+
+        // Assert
+        Assert.assertTrue((direccionEsperada.posicionRelativa()).igualA(direccionObtenida.posicionRelativa()));
+    }
+
+    @Test
+    public void posicion55PosicionRelativaAPosicion56DevuelveNorte() {
+        // Arrange
+        Posicion posicion = new Posicion(5,5);
+        Posicion otraPosicion = new Posicion(4,5);
+        Direccion direccionEsperada = new Norte();
+        // Act
+        Direccion direccionObtenida = posicion.posicionRelativaA(otraPosicion);
+
+        // Assert
+        Assert.assertTrue((direccionEsperada.posicionRelativa()).igualA(direccionObtenida.posicionRelativa()));
+    }
 }
