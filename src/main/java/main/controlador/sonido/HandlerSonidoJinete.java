517537dbca8373cda49aa9a42c66034991e8246a
ctorales <crist.torales@gmail.com>
2019-12-12 21:00:59 -0300

    Solo pruebas por el mal funcionamiento de Javafx.

--- src/main/java/main/controlador/sonido/HandlerSonidoJinete.java
+++ src/main/java/main/controlador/sonido/HandlerSonidoJinete.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class HandlerSonidoJinete {
     private static HandlerSonido sonidoCreacion =
-            new HandlerSonido(new File("src/recursos/sonidos/mangonelselect.wav"));
+            new HandlerSonido(new File("file:src/recursos/sonidos/mangonelselect.wav"));
 
     static public void reproducirSonidoSeleccion(){
         sonidoCreacion.reproducirSonido();
