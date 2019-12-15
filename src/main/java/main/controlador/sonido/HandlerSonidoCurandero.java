a507db33385581f793339ea38b0d6cb170160ed3
ctorales <crist.torales@gmail.com>
2019-12-15 16:12:08 -0300

    Se corrigieron errores de sonido.

--- src/main/java/main/controlador/sonido/HandlerSonidoCurandero.java
+++ src/main/java/main/controlador/sonido/HandlerSonidoCurandero.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class HandlerSonidoCurandero {
     private static HandlerSonido sonidoCreacion =
-            new HandlerSonido(new File("file:src/recursos/sonidos/priest_converting2.wav"));
+            new HandlerSonido(new File("src/recursos/sonidos/priest_converting2.wav"));
 
     static public void reproducirSonidoSeleccion(){
         sonidoCreacion.reproducirSonido();
--- src/main/java/main/controlador/sonido/HandlerSonidoSoldado.java
+++ src/main/java/main/controlador/sonido/HandlerSonidoSoldado.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class HandlerSonidoSoldado {
     private static HandlerSonido sonidoCreacion =
-            new HandlerSonido(new File("file:src/recursos/sonidos/military_spawn.wav"));
+            new HandlerSonido(new File("src/recursos/sonidos/military_spawn.wav"));
 
     static public void reproducirSonidoSeleccion(){
         sonidoCreacion.reproducirSonido();
