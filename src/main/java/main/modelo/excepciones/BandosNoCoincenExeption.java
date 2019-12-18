7f9470473c25a3d537c3f0834c2fbacfb211a232
joaco <joacoelordi@gmail.com>
2019-12-17 21:51:56 -0300

    agrega imagen a ventana ganar y error

--- src/main/java/main/modelo/excepciones/BandosNoCoincenExeption.java
+++ src/main/java/main/modelo/excepciones/BandosNoCoincenExeption.java
@@ -1,4 +1,8 @@
 package main.modelo.excepciones;
 
-public class BandosNoCoincenExeption extends RuntimeException {
+public class BandosNoCoincenExeption extends ExcepcionEnJuego {
+    @Override
+    public String mensajeDeError() {
+        return "Los bandos no coinciden";
+    }
 }
