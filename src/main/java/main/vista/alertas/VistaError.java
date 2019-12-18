6ca1ae958ec71a2346128bd1aaa22ce562b75564
joaco <joacoelordi@gmail.com>
2019-12-17 21:33:45 -0300

    agrega imagen a ventana ganar y error

--- src/main/java/main/vista/alertas/VistaError.java
+++ src/main/java/main/vista/alertas/VistaError.java
@@ -1,6 +1,8 @@
 package main.vista.alertas;
 
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import main.modelo.excepciones.ExcepcionEnJuego;
 
 public class VistaError extends Alert {
@@ -8,6 +10,10 @@ public class VistaError extends Alert {
         super(Alert.AlertType.ERROR);
         this.setHeaderText("Jugada no permitida");
         this.setContentText(excepcion.mensajeDeError());
+        Image image = new Image("file:src/recursos/img/youBetterWatchYourMouse.png",200,200,true,true);
+        ImageView imageView = new ImageView(image);
+        this.setGraphic(imageView);
+
         this.showAndWait();
     }
 }
--- src/main/java/main/vista/alertas/VistaJugadorGano.java
+++ src/main/java/main/vista/alertas/VistaJugadorGano.java
@@ -1,7 +1,13 @@
 package main.vista.alertas;
 
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import main.modelo.juego.Jugador;
+
+import java.io.File;
 
 public class VistaJugadorGano extends Alert {
 
@@ -9,6 +15,12 @@ public class VistaJugadorGano extends Alert {
         super(Alert.AlertType.ERROR);
         this.setHeaderText("Victoria!");
         this.setContentText("Enhorabuena ".concat(jugador.nombre()).concat(", has ganado!"));
+        Image image = new Image("file:src/recursos/img/hasGanadoLaPartida.jpg",200,200,true,true);
+        ImageView imageView = new ImageView(image);
+        this.setGraphic(imageView);
+        Media media = new Media(new File("src/recursos/sonidos/AgeofEmpires2Maintheme-8 bit.mp3").toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
         this.showAndWait();
     }
 }
